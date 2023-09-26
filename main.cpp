7e2c5776e49d64d55723ff00f1a910d8603e4661
KristinaRiabova <kristinaer304@gmail.com>
2023-09-26 14:14:49 +0300

    Function LoadTextFromFile SaveTextToFile

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <fstream>
 
 class Line {
 public:
@@ -117,6 +118,37 @@ public:
         }
         return count;
     }
+    void LoadTextFromFile(const std::string& filename) {
+        std::ifstream file(filename);
+        if (file.is_open()) {
+            lines.clear();  // Clear existing text
+            std::string line;
+            while (std::getline(file, line)) {
+                lines.emplace_back(line);
+            }
+            file.close();
+            history.clear();
+            history.push_back(lines);
+            historyIndex = 0;
+            std::cout << "Text loaded from file: " << filename << std::endl;
+        } else {
+            std::cout << "Error opening file: " << filename << std::endl;
+        }
+    }
+
+    void SaveTextToFile(const std::string& filename) {
+        std::ofstream file(filename);
+        if (file.is_open()) {
+            for (const auto& line : lines) {
+                file << line.GetText() << std::endl;
+            }
+            file.close();
+            std::cout << "Text saved to file: " << filename << std::endl;
+        } else {
+            std::cout << "Error opening file for saving: " << filename << std::endl;
+        }
+    }
+
     void InsertSubstring(size_t lineIndex, size_t index, const std::string& substring) {
         if (lineIndex < lines.size()) {
             lines[lineIndex].Insert(index, substring);
@@ -229,6 +261,22 @@ int main() {
                 std::getline(std::cin, substring);
                 size_t count = storage.SearchForText(substring);
                 std::cout << "Found " << count << " occurrences of '" << substring << "'" << std::endl;
+                break;
+            }
+            case 12: {
+                std::cout << "Enter filename to load text from: ";
+                std::string filename;
+                std::cin.ignore();
+                std::getline(std::cin, filename);
+                storage.LoadTextFromFile(filename);
+                break;
+            }
+            case 13: {
+                std::cout << "Enter filename to save text to: ";
+                std::string filename;
+                std::cin.ignore();
+                std::getline(std::cin, filename);
+                storage.SaveTextToFile(filename);
                 break;
             }
             case 14: {
