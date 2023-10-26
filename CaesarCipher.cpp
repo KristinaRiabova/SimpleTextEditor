3297fd4e041f438dc394bf54a7b9e4afc251cf19
KristinaRiabova <kristinaer304@gmail.com>
2023-10-26 21:29:34 +0300

    moved the logic for loading/unloading the library to constructor and destructor

--- CaesarCipher.cpp
+++ CaesarCipher.cpp
@@ -1,42 +1,48 @@
-#include "CaesarCipher.h"
 #include <dlfcn.h>
+#include "CaesarCipher.h"
 
-CaesarCipher::CaesarCipher() {}
+
+void* CaesarCipher::handle = nullptr;
+
+CaesarCipher::CaesarCipher() {
+
+    handle = dlopen("/Users/kristina_mbp/CLionProjects/CaesarEncryptionAlgorithm/caesar.dylib", RTLD_LAZY);
+    if (!handle) {
+        handle = nullptr;
+    }
+}
+
+CaesarCipher::~CaesarCipher() {
+
+    if (handle) {
+        dlclose(handle);
+    }
+}
 
 char* CaesarCipher::Encrypt(const char* text, int key) {
-    void* handle = dlopen("/Users/kristina_mbp/CLionProjects/CaesarEncryptionAlgorithm/caesar.dylib", RTLD_LAZY);
     if (!handle) {
-
         return nullptr;
     }
 
     char* (*encryptFunction)(const char*, int) = (char* (*)(const char*, int))dlsym(handle, "encrypt");
     if (!encryptFunction) {
-
-        dlclose(handle);
         return nullptr;
     }
 
     char* encryptedText = encryptFunction(text, key);
-    dlclose(handle);
     return encryptedText;
 }
 
 char* CaesarCipher::Decrypt(const char* text, int key) {
-    void* handle = dlopen("/Users/kristina_mbp/CLionProjects/CaesarEncryptionAlgorithm/caesar.dylib", RTLD_LAZY);
     if (!handle) {
-
         return nullptr;
     }
 
     char* (*decryptFunction)(const char*, int) = (char* (*)(const char*, int))dlsym(handle, "decrypt");
     if (!decryptFunction) {
-
-        dlclose(handle);
         return nullptr;
     }
 
     char* decryptedText = decryptFunction(text, key);
-    dlclose(handle);
     return decryptedText;
 }
--- CaesarCipher.h
+++ CaesarCipher.h
@@ -4,8 +4,13 @@
 class CaesarCipher {
 public:
     CaesarCipher();
+    ~CaesarCipher(); // Explicit destructor declaration
+
     char* Encrypt(const char* text, int key);
     char* Decrypt(const char* text, int key);
+
+private:
+    static void* handle; // Private static member variable
 };
 
 #endif
